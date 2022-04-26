b8421b8a729d3d02b160332aa362b684ef61642c
cucumparty <100706133+cucumparty@users.noreply.github.com>
2022-04-26 16:29:45 +0300

    Update 4-2.py

--- 4-2.py
+++ 4-2.py
@@ -1,25 +1,28 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(dac, GPIO.out)
+GPIO.setup(dac, GPIO.OUT)
 
 def decimal2binary(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
 try:
     while True :
-        period = 10
+        period = 20
         i = 0
-        while i < 256 :
+        for i in range(255) :
             GPIO.output(dac, decimal2binary(i))
             i+=1
+            print (i)
             time.sleep (period / 512)
-        while i > 0 :
+
+        while i >= 0 :
             GPIO.output(dac, decimal2binary(i))
             i-=1
+            print (i)
             time.sleep (period / 512)
 
 finally:
